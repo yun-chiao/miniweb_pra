7c650e25e9ae39fd81331c406795fec84f93ff61
yunchiao <jojo050872@gmail.com>
2021-10-05 01:45:16 +0800

    step-com

--- steps/script.js
+++ steps/script.js
@@ -1,21 +1,36 @@
 var line = document.querySelector(".act-line");
 var prev = document.querySelector(".prev");
 var next = document.querySelector(".next");
+var doms = document.querySelectorAll(".circle");
 var n = 3;
 var current = 1;
 var percent = 100 / n;
+
+
 next.addEventListener(("click"),() => {
-    current = current + 1;
-    line.style.width = `${current*100/3}%`;
+    if(current === 0){
+        prev.classList.remove("disable");
+    }
+    if(current !== n){
+        current = current + 1;
+        line.style.width = `${current*100/3}%`;
+        doms[current].classList.add("active");
+    }
     if(current === n){
         next.classList.add("disable");
     }
 })
 
+
 prev.addEventListener(("click"),() => {
-    current = current - 1;
-    line.style.width = `${current*100/3}%`;
-    if(current === 0){
-        prev.classList.add("disable");
+    if(current === n){
+        next.classList.remove("disable");
     }
+    if(current !== 0){
+        current = current - 1;
+        line.style.width = `${current*100/3}%`;
+        doms[current+1].classList.remove("active");
+    }
+    if(current === 0)prev.classList.add("disable");
+
 })
