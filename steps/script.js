16a7df37d7c4cadb190ea9cc46ba7e9369d9872e
yunchiao <jojo050872@gmail.com>
2021-10-05 01:14:05 +0800

    button

--- steps/script.js
+++ steps/script.js
@@ -1,11 +1,21 @@
 var line = document.querySelector(".act-line");
 var prev = document.querySelector(".prev");
 var next = document.querySelector(".next");
-
+var n = 3;
+var current = 1;
+var percent = 100 / n;
 next.addEventListener(("click"),() => {
-    
+    current = current + 1;
+    line.style.width = `${current*100/3}%`;
+    if(current === n){
+        next.classList.add("disable");
+    }
 })
 
 prev.addEventListener(("click"),() => {
-    
+    current = current - 1;
+    line.style.width = `${current*100/3}%`;
+    if(current === 0){
+        prev.classList.add("disable");
+    }
 })
