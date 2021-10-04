7011c6f8884b83718bcf50cf0e30b79df08e5b55
yunchiao <jojo050872@gmail.com>
2021-10-04 11:02:23 +0800

    FAQ

--- FAQ/script.js
+++ FAQ/script.js
@@ -1,1 +1,16 @@
+var doms = document.querySelectorAll(".faq-toggle");
 
+console.log(doms)
+doms.forEach((element) => {
+
+    element.addEventListener("click",() => {
+        if(element.classList.contains("active")){
+            element.parentNode.classList.remove("active");
+            element.classList.remove("active");
+        }
+        else{
+            element.parentNode.classList.add("active");
+            element.classList.add("active");
+        }
+    })
+})
