7c475b29110211ed71bab76b3f46330938d9dad0
yunchiao <jojo050872@gmail.com>
2021-10-05 01:05:29 +0800

    step's html

---
+++ steps/script.js
@@ -0,0 +1,11 @@
+var line = document.querySelector(".act-line");
+var prev = document.querySelector(".prev");
+var next = document.querySelector(".next");
+
+next.addEventListener(("click"),() => {
+    
+})
+
+prev.addEventListener(("click"),() => {
+    
+})
